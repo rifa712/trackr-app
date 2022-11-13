8a8cdc94f1de8e61eb494fa65a6671322cde4bc9
Muhammad Rifa A <rifaalghan@gmail.com>
2022-11-13 13:02:04 +0700

    try deploy

--- backend/config/firebase-admin.config.js
+++ backend/config/firebase-admin.config.js
@@ -4,17 +4,27 @@ require('dotenv').config()
 admin.initializeApp({
   credential: admin.credential.cert({
     type: 'service_account',
-    project_id: process.env.PROJECT_ID,
-    private_key_id: process.env.PRIVATE_ID_KEY,
+    project_id: process.env.PROJECT_ID
+      ? JSON.parse(process.env.PROJECT_ID)
+      : undefined,
+    private_key_id: process.env.PRIVATE_ID_KEY
+      ? JSON.parse(process.env.PRIVATE_ID_KEY)
+      : undefined,
     private_key: process.env.PRIVATE_KEY
       ? JSON.parse(process.env.PRIVATE_KEY)
       : undefined,
-    client_email: process.env.CLIENT_EMAIL,
-    client_id: process.env.CLIENT_ID,
+    client_email: process.env.CLIENT_EMAIL
+      ? JSON.parse(process.env.CLIENT_EMAIL)
+      : undefined,
+    client_id: process.env.CLIENT_ID
+      ? JSON.parse(process.env.CLIENT_ID)
+      : undefined,
     auth_uri: 'https://accounts.google.com/o/oauth2/auth',
     token_uri: 'https://oauth2.googleapis.com/token',
     auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
-    client_x509_cert_url: process.env.CLIENT_X509_CERT_URL,
+    client_x509_cert_url: process.env.CLIENT_X509_CERT_URL
+      ? JSON.parse(process.env.CLIENT_X509_CERT_URL)
+      : undefined,
   }),
 })
 
